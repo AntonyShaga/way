7b761e8e207af577d76f6152a8e6c5b514f49b02
Antony Shaga <antonyshaga@gmail.com>
2023-04-26 17:37:44 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR96IDjbK5JHDBsd1jgKESzH1Vx73c3Yp7UvbFFqVLJEmEj3qo73939yR8z4feyY_DT0c8&usqp=CAU'/>
+        </header>
+    )
+}
